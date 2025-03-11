0c963d043354064997520b876934370be1a624c5
NatalieGichuki <nataliegichuki@gmail.com>
2025-03-11 13:27:18 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(){
+    cats.push ("Ralph");
+}
+
+function destructivelyPrependCat(){
+    cats.unshift ("Bob");
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift ();
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function appendCat(){
+    return [...cats, "Broom"];
+}
+
+function prependCat(){
+    return ["Arnold", ...cats];
+}
+
+
+function removeLastCat() {
+    return cats.slice(0, -1); 
+  }
+  
+  function removeFirstCat() {
+    return cats.slice(1); 
+  }
+  
